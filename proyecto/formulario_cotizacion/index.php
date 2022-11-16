5416f8649c6a466088e20fd0752ba69c059c3da1
Yeferson Garcia <yeferson.garcia@payu.com>
2022-11-16 02:12:07 -0500

    Fix issue related with form cotizaciones and issue with connect to data base

--- proyecto/formulario_cotizacion/index.php
+++ proyecto/formulario_cotizacion/index.php
@@ -6,14 +6,68 @@
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
+	
     <form method="post">
-    	<h1>¡Suscribete!</h1>
+    	<h1>¡Cotiza con nosotros!</h1>
     	<input type="text" name="name" placeholder="Nombre completo">
+    	<input type="text" name="cedula" placeholder="Cedula">
+    	<input type="text" name="direccion" placeholder="Direccion">
+    	<input type="text" name="celular" placeholder="Celular">
     	<input type="email" name="email" placeholder="Email">
+    	<input type="text" name="interested_mtcl" placeholder="Moto de interes">
     	<input type="submit" name="register">
+    	<input type="reset" name="erase">
     </form>
         <?php 
         include("registrar.php");
         ?>
+
+
+    <!-- CAMBIOS DANIELA-->
+
+    <div class="textoredes">
+        <h1>SBK RACING</h1>
+        <p>Copyright © 2022 SBK Motor Racing – A Sole Shareholder Company - A Company subject to the Management and
+            Coordination
+            activities of AUDI AG. All rights reserved. VAT 05113870967</p>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="">
+        <ul>
+            <div class="redes">
+                <h3>Siguenos</h3>
+                <hr>
+                <ul class="social">
+                    <li><a href="https://www.facebook.com/" class="fa fa-facebook"></a></li>
+                    <li><a href="https://twitter.com/" class="fa fa-twitter"></a></li>
+                    <li><a href="https://www.instagram.com/" class="fa fa-instagram"></a></li>
+                    <li><a href="https://co.pinterest.com/" class="fa fa-pinterest"></a></li>
+                    <li><a href="https://www.youtube.com/" class="fa fa-youtube"></a></li>
+                    <li><a href="https://mail.google.com/" class="fa fa-envelope-o"></a></li>
+                </ul>
+
+            </div>
+
+            <div class="contactos">
+                <h3>Contáctenos</h3>
+                <hr>
+                <ul class="social">
+                    <li><a href="#" class="fa fa-phone"></a></li>
+                    <p>3142844877 - 01800</p>
+                </ul>
+                <ul class="social">
+                    <li><a href="#" class="fa fa-envelope-o"></a></li>
+                    <p>superbike@gmail.com</p>
+                </ul>
+            </div>
+        </ul>
+        <hr>
+        
+    </div>
+		
 </body>
 </html>
---
+++ proyecto/formulario_cotizacion/registrar.php
@@ -0,0 +1,32 @@
+<?php 
+
+include("con_db.php");
+
+if (isset($_POST['register'])) {
+    if (strlen($_POST['name']) >= 1
+             && strlen($_POST['email']) >= 1) {
+	    $name = trim($_POST['name']);
+	    $cedula = trim($_POST['cedula']);
+	    $direccion = trim($_POST['direccion']);
+	    $celular = trim($_POST['celular']);
+	    $email = trim($_POST['email']);
+	    $fechareg = date("d/m/y");
+	    $consulta = "INSERT INTO cotizaciones(name, cedula, address, phone_number, email, fecha_reg) VALUES ('$name','$cedula','$direccion','$celular','$email','$fechareg')";
+	    $resultado = mysqli_query($conex,$consulta);
+	    if ($resultado) {
+	    	?> 
+	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
+           <?php
+	    } else {
+	    	?> 
+	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
+           <?php
+	    }
+    }   else {
+	    	?> 
+	    	<h3 class="bad">¡Por favor complete los campos!</h3>
+           <?php
+    }
+}
+
+?>
